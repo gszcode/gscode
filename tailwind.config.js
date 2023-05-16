c7009dbd78334f8d5aecca764bd11b512ffeb169
Gabriel <gszcode@gmail.com>
2023-05-16 16:59:33 -0300

    colores agregaos a tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      'color-text': '#4f2f99',
+      'color-button': '#3c94cb',
+      'color-header': '#fff'
+    },
+    extend: {}
+  },
+  plugins: []
+}
