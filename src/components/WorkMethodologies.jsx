e55e290c20f869e24cee8393d18c76f689eb7e0b
Gabriel <gszcode@gmail.com>
2023-05-20 20:30:30 -0300

    comnponente refactorizado

--- src/components/WorkMethodologies.jsx
+++ src/components/WorkMethodologies.jsx
@@ -1,21 +1,8 @@
 import AccordionMethodogies from './AccordionMethodogies'
 
-const data = {
-  titleOne: 'Tiempo y asertividad',
-  titleTwo: 'Lista de candidatos',
-  titleThree: 'Seguimiento de candidatos',
-  descriptionOne:
-    'Presentación de candidatos entre la 2da y 3er semana luego de dar luz verde al inicio del proceso. Esto se logra gracias a nuestra especialización.',
-  descriptionTwo:
-    'Se presentan de 4 a 6 candidatos alineados a las necesidades del cliente.',
-  descriptionThree:
-    'Contamos con un informe de seguimiento para evaluar la adaptación y comodidad del candidato durante los primeros meses en la compañía.',
-  value: 'work'
-}
-
 const WorkMethodologies = () => {
   return (
-    <section className="p-3 bg-background">
+    <section className="p-3">
       <div>
         <h2 className="font-semibold text-2xl text-center m-4">
           Metolología de trabajo
@@ -26,7 +13,7 @@ const WorkMethodologies = () => {
         </p>
       </div>
       <div className="my-20">
-        <AccordionMethodogies data={data} />
+        <AccordionMethodogies methodologie="work" />
       </div>
     </section>
   )
