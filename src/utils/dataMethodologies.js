cbb50bd7dd8bb1ed040dafb4cc0a4afca1d5609e
Gabriel <gszcode@gmail.com>
2023-05-20 20:33:08 -0300

    datos para acordion modularizado y extraido

---
+++ src/utils/dataMethodologies.js
@@ -0,0 +1,48 @@
+export const methodologies = {
+  work: [
+    {
+      id: 0,
+      title: 'Tiempo y asertividad',
+      description:
+        'Presentación de candidatos entre la 2da y 3er semana luego de dar luz verde al inicio del proceso. Esto se logra gracias a nuestra especialización.',
+      iconClass: 'fa-regular fa-calendar-days text-2xl'
+    },
+    {
+      id: 1,
+      title: 'Lista de candidatos',
+      description:
+        'Se presentan de 4 a 6 candidatos alineados a las necesidades del cliente.',
+      iconClass: 'fa-solid fa-users text-2xl'
+    },
+    {
+      id: 2,
+      title: 'Seguimiento de candidatos',
+      description:
+        'Contamos con un informe de seguimiento para evaluar la adaptación y comodidad del candidato durante los primeros meses en la compañía.',
+      iconClass: 'fa-regular fa-file text-2xl'
+    }
+  ],
+  search: [
+    {
+      id: 0,
+      title: 'Headhunting',
+      description:
+        'Nuestro core business es ser especialistas en salir a buscar candidatos en el mercado laboral.',
+      iconClass: 'fa-solid fa-magnifying-glass text-2xl'
+    },
+    {
+      id: 1,
+      title: 'Base de datos propia',
+      description:
+        'Invertimos y apostamos en las últimas tecnologías las cuales nos permiten contar con información actualizada de profesionales específicos de nuestras distintas áreas de expertise.',
+      iconClass: 'fa-solid fa-database text-2xl'
+    },
+    {
+      id: 2,
+      title: 'Portales utilizados',
+      description:
+        'Linkedin premium, Licencia de recruiter, Portales de empleo',
+      iconClass: 'fa-brands fa-linkedin-in text-2xl'
+    }
+  ]
+}
