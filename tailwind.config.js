8a706b5dca34f5647e9c79160973f569423f670f
Gabriel <gszcode@gmail.com>
2023-05-20 20:30:43 -0300

    color negro agregado

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      background: '#eae4f720',
       'primary-color': '#4f2f99',
       'second-color': '#3c94cb',
       'second-grey': '#7A7A7A',
       'third-color': '#3c94cb5a',
       white: '#fff',
+      black: '#000',
       grey: '#F9F9F9',
       transparent: 'transparent'
     },
