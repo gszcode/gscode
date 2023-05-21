58e802e1ccd83b811f75d42eb62ebf875d63bd85
Gabriel <gszcode@gmail.com>
2023-05-20 22:17:44 -0300

    articulos de blog modularizado

---
+++ src/components/ArticleBlog.jsx
@@ -0,0 +1,19 @@
+import blogImg from '../img/header-01.webp'
+
+const ArticleBlog = () => {
+  return (
+    <article>
+      <div className="w-full h-40">
+        <img className="w-full h-full object-cover" src={blogImg} />
+      </div>
+      <h3 className="text-lg font-semibold my-3">Lorem ipsum dolor</h3>
+      <p className="my-3 text-sm text-second-grey">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis
+        minus inventore quaerat distinctio
+      </p>
+      <button className="font-semibold text-primary-color">Leer más »</button>
+    </article>
+  )
+}
+
+export default ArticleBlog
