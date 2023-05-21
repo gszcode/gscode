0d289d5c3a309d89a1ce631c7d9691e3a9e1f7f7
Gabriel <gszcode@gmail.com>
2023-05-20 22:16:50 -0300

    componente de metodologias creado

---
+++ src/components/Methodologies.jsx
@@ -0,0 +1,14 @@
+import SearchMethodologies from './SearchMetodologies'
+import WorkMethodologies from './WorkMethodologies'
+
+const Methodologies = () => {
+  return (
+    <div id="methodologies">
+      <WorkMethodologies />
+      <div className="div" />
+      <SearchMethodologies />
+    </div>
+  )
+}
+
+export default Methodologies
