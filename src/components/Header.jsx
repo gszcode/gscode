5530ddca2dc5b02a3ed6d21ae302283482c3fb03
Gabriel <gszcode@gmail.com>
2023-05-20 22:16:01 -0300

    links funcionando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header className="z-10 flex justify-between items-center p-3 fixed bg-white w-full h-20">
-      <a href="/" className="flex items-center w-18 h-10">
+      <div className="flex items-center w-18 h-10">
         <img
           id="logo-title"
           className="w-full h-full"
@@ -21,7 +21,7 @@ const Header = () => {
         <h1 className="ml-2 text-lg font-semibold text-primary-color">
           GSCODE
         </h1>
-      </a>
+      </div>
       <div className="logo-sm cursor-pointer hover:text-primary-color">
         <button onClick={handleClick}>
           {btnMenu === false ? (
@@ -36,20 +36,28 @@ const Header = () => {
           btnMenu === false ? 'hidden' : 'block'
         } nav-transition font-light`}
       >
-        <ul className="logo-lg">
-          <li className="my-4 hover:text-primary-color cursor-pointer">
+        <ul className="logo-lg flex flex-col items-start gap-4">
+          <a href="#home" className="hover:text-primary-color cursor-pointer">
             Inicio
-          </li>
-          <li className="my-4 hover:text-primary-color cursor-pointer">
+          </a>
+          <a
+            href="#methodologies"
+            className="hover:text-primary-color cursor-pointer"
+          >
             Metodologías
-          </li>
-          <li className="my-4 hover:text-primary-color cursor-pointer">
+          </a>
+          <a href="#about" className="hover:text-primary-color cursor-pointer">
             Nosotros
-          </li>
-          <li className="my-4 hover:text-primary-color cursor-pointer">Blog</li>
-          <li className="my-4 hover:text-primary-color cursor-pointer font-semibold">
+          </a>
+          <a href="#blog" className="hover:text-primary-color cursor-pointer">
+            Blog
+          </a>
+          <a
+            href="#contact"
+            className="hover:text-primary-color cursor-pointer font-semibold"
+          >
             Contacto
-          </li>
+          </a>
         </ul>
       </nav>
     </header>
