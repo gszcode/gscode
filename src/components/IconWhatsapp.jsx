a713179f7c41dfff906b1a9ee94dee012deffac2
Gabriel <gszcode@gmail.com>
2023-05-23 23:47:50 -0300

    componente icono de whatsapp

---
+++ src/components/IconWhatsapp.jsx
@@ -0,0 +1,12 @@
+const IconWhatsapp = () => {
+  return (
+    <a
+      href="https://wa.link/nlrmhm"
+      className="fixed bottom-5 right-5 z-10 bg-whatsapp rounded-[50%] py-[10px] px-[16px] cursor-pointer hover:bg-whatsappHover hover:scale-110 lg:py-[14px] lg:px-[20px]"
+    >
+      <i className="fa-brands fa-whatsapp text-3xl text-white lg:text-4xl"></i>
+    </a>
+  )
+}
+
+export default IconWhatsapp
