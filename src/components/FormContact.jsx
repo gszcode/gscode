d15fb781e7c42e97bb64e9608c851bf2c66d798d
Gabriel <gszcode@gmail.com>
2023-05-25 00:05:06 -0300

    formulario controlado para envio de datos

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,21 +1,73 @@
+import { useState } from 'react'
 import CustomInput from './CustomInput'
+import { sendEmail } from '../utils/sendEmail'
+
+const initialForm = {
+  userName: '',
+  userPhone: '',
+  userEmail: '',
+  subjectEmail: '',
+  message: ''
+}
 
 const FormContact = () => {
+  const [contact, setContact] = useState(initialForm)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setContact({ ...contact, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    sendEmail(contact)
+    setContact(initialForm)
+  }
+
   return (
     <form
+      onSubmit={handleSubmit}
       className="flex flex-col gap-2 w-full p-4 text-second-grey justify-center md:flex-row md:flex-wrap"
       data-aos="fade-up"
       data-aos-duration="2000"
     >
-      <CustomInput placeholder="Nombre" type="text" />
-      <CustomInput placeholder="Teléfono" type="tel" />
-      <CustomInput placeholder="Email" type="email" />
-      <CustomInput placeholder="Asunto" type="text" />
+      <CustomInput
+        placeholder="Nombre"
+        type="text"
+        handleChange={handleChange}
+        value={contact.userName}
+        name="userName"
+      />
+      <CustomInput
+        placeholder="Teléfono"
+        type="tel"
+        handleChange={handleChange}
+        value={contact.userPhone}
+        name="userPhone"
+      />
+      <CustomInput
+        placeholder="Email"
+        type="email"
+        handleChange={handleChange}
+        value={contact.userEmail}
+        name="userEmail"
+      />
+      <CustomInput
+        placeholder="Asunto"
+        type="text"
+        handleChange={handleChange}
+        value={contact.subjectEmail}
+        name="subjectEmail"
+      />
       <div className="textarea">
         <textarea
           className="outline-none w-full h-40 resize-none px-3 pt-2 font-light md:w-[80vw] lg:w-[63vw]"
           placeholder="Mensaje"
           required
+          onChange={handleChange}
+          value={contact.message}
+          name="message"
         />
       </div>
       <div className="w-60 flex justify-start md:justify-center lg:w-full btn-contact">
