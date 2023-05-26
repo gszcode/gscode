8f202725b4a78b4904e3ed6894ea380f8c9ca934
Gabriel <gszcode@gmail.com>
2023-05-25 21:19:22 -0300

    ruteo de paginas realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Background from './components/Background'
-import Layout from './components/Layout'
-import About from './components/About'
-import ChooseUs from './components/ChooseUs'
-import Blog from './components/Blog'
-import Methodologies from './components/Methodologies'
-import Contact from './components/Contact'
-import AOS from 'aos'
 import 'aos/dist/aos.css'
-import IconWhatsapp from './components/IconWhatsapp'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from './components/Layout'
+import AOS from 'aos'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import BlogId from './pages/BlogId'
+import AllBlogs from './pages/AllBlogs'
 AOS.init()
 
 function App() {
   return (
     <Layout>
-      <Background />
-      <Methodologies />
-      <About />
-      <ChooseUs />
-      <Blog />
-      <Contact />
-      <IconWhatsapp />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Login />} />
+        <Route path="/blog/:id" element={<BlogId />} />
+        <Route path="/blogs" element={<AllBlogs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Layout>
   )
 }
