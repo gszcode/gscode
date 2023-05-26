db63d921deb75dc8254a24637af31ca05a6e17ce
Gabriel <gszcode@gmail.com>
2023-05-25 21:18:54 -0300

    aria-label agregado a icono

--- src/components/IconWhatsapp.jsx
+++ src/components/IconWhatsapp.jsx
@@ -3,6 +3,7 @@ const IconWhatsapp = () => {
     <a
       href="https://wa.link/nlrmhm"
       className="fixed bottom-5 right-5 z-10 bg-whatsapp rounded-[50%] py-[10px] px-[16px] cursor-pointer hover:bg-whatsappHover hover:scale-110 lg:py-[14px] lg:px-[20px]"
+      aria-label="Enlace WhatsApp"
     >
       <i className="fa-brands fa-whatsapp text-3xl text-white lg:text-4xl"></i>
     </a>
