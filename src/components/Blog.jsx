fc1a733e2d32b14ec695baf5411baaf69c4e3dd5
Gabriel <gszcode@gmail.com>
2023-05-25 21:20:18 -0300

    estilos modificados

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import ArticleBlog from './ArticleBlog'
 
 const Blog = () => {
@@ -6,12 +7,13 @@ const Blog = () => {
       <div className="block">
         <h2 className="text-2xl font-bold text-center md:text-4xl">Blog</h2>
         <h4 className="text-second-grey mt-4 text-center md:text-lg">
-          Últimas novedades en nuestro blog
+          Últimas novedades en nuestro blog{' '}
+          <Link to="/blogs" className="text-sm text-second-color">
+            (<span className="line">Ver todos los Blogs</span>)
+          </Link>
         </h4>
       </div>
       <div className="p-4 flex flex-col items-center justify-evenly h-full gap-5 md:flex-row">
-        <ArticleBlog />
-        <ArticleBlog />
         <ArticleBlog />
       </div>
     </section>
