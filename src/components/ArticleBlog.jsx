b022e0f7cb2b3bc052ae5d71af4cf73c48db0ea4
Gabriel <gszcode@gmail.com>
2023-05-30 14:48:39 -0300

    consulta a la api por blogs

--- src/components/ArticleBlog.jsx
+++ src/components/ArticleBlog.jsx
@@ -9,7 +9,7 @@ const ArticleBlog = () => {
 
   const blogs =
     allBlogs.length > 3
-      ? allBlogs.splice(allBlogs.length - 3, allBlogs.length - 1)
+      ? allBlogs.slice(allBlogs.length - 3, allBlogs.length - 1 + 1)
       : allBlogs
 
   useEffect(() => {
@@ -24,22 +24,27 @@ const ArticleBlog = () => {
         </p>
       ) : (
         blogs.map((art) => (
-          <article key={art.id} data-aos="fade-up" data-aos-duration="2000">
+          <article
+            key={art.id}
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            className="w-[250px] md:w-[300px] py-2 px-3 shadow-sm"
+          >
             <div className="w-full h-56 lg:h-60">
               <img
                 className="w-full h-full object-cover"
-                src={art.img}
-                alt={art.title}
+                src={art.image}
+                alt={`${art.title.slice(0, 10)}...`}
               />
             </div>
             <h3 className="text-lg font-semibold my-3 lg:text-2xl">
-              {art.title}
+              {`${art.title.slice(0, 15)}...`}
             </h3>
-            <p className="my-3 text-sm text-second-grey lg:text-base">
-              {art.description}
+            <p className="my-3 text-sm text-second-grey lg:text-base ">
+              {`${art.description.slice(0, 20)}...`}
             </p>
             <Link
-              to={`/blog/${art.id}`}
+              to={`/blog/${art._id}`}
               className="font-semibold text-primary-color lg:text-lg"
             >
               Leer más »
