e941545fae33ab3113874cc435aa7642c094bda9
Gabriel <gszcode@gmail.com>
2023-05-30 20:07:11 -0300

    logo entereo linkeado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,20 @@ import NavbarSmall from './NavbarSmall'
 const Header = () => {
   return (
     <header className="z-10 flex justify-between items-center p-3 fixed bg-white w-full h-20 shadow-sm">
-      <div className="flex items-center w-18 h-10 lg:w-36 lg:h-16">
-        <img
-          id="logo-title"
-          className="w-full h-full"
-          src={logo}
-          alt="Logo"
-          title="Logo"
-        />
-        <Link to="/">
+      <Link to="/">
+        <div className="flex items-center w-18 h-10 lg:w-36 lg:h-16">
+          <img
+            id="logo-title"
+            className="w-full h-full"
+            src={logo}
+            alt="Logo"
+            title="Logo"
+          />
           <h1 className="ml-2 text-lg font-semibold text-primary-color md:text-2xl lg:text-3xl">
             GSCODE
           </h1>
-        </Link>
-      </div>
+        </div>
+      </Link>
       <NavbarSmall />
       <NavbarLong />
     </header>
