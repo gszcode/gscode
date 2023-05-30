8cac8e93dc215cd15e4d1db6251e18590d6ae464
Gabriel <gszcode@gmail.com>
2023-05-29 22:59:08 -0300

    consulta de blogs al backend

--- src/components/ArticleBlog.jsx
+++ src/components/ArticleBlog.jsx
@@ -1,84 +1,52 @@
 import { Link } from 'react-router-dom'
-import blogImg from '../img/header-01.webp'
-
-const imgs = [
-  {
-    id: 1,
-    img: blogImg,
-    title: 'Lorem ipsum dolor',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 2,
-    img: blogImg,
-    title: 'Lorem ipsum dolor',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 3,
-    img: blogImg,
-    title: 'Lorem ipsum dolor',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 4,
-    img: blogImg,
-    title: 'hola',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 5,
-    img: blogImg,
-    title: 'bebe',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 6,
-    img: blogImg,
-    title: 'cita',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  },
-  {
-    id: 7,
-    img: blogImg,
-    title: 'asiii',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veritatis minus inventore quaerat distinctio'
-  }
-]
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { getBlogs } from '../slides/blog/blogSlice'
 
 const ArticleBlog = () => {
+  const { allBlogs } = useSelector((state) => state.blogAdmin)
+  const dispatch = useDispatch()
+
+  const blogs =
+    allBlogs.length > 3
+      ? allBlogs.splice(allBlogs.length - 3, allBlogs.length - 1)
+      : allBlogs
+
+  useEffect(() => {
+    dispatch(getBlogs())
+  }, [dispatch])
+
   return (
     <>
-      {imgs.splice(imgs.length - 3, imgs.length - 1).map((art) => (
-        <article key={art.id} data-aos="fade-up" data-aos-duration="2000">
-          <div className="w-full h-56 lg:h-60">
-            <img
-              className="w-full h-full object-cover"
-              src={art.img}
-              alt={art.title}
-            />
-          </div>
-          <h3 className="text-lg font-semibold my-3 lg:text-2xl">
-            {art.title}
-          </h3>
-          <p className="my-3 text-sm text-second-grey lg:text-base">
-            {art.description}
-          </p>
-          <Link
-            to={`/blog/${art.id}`}
-            className="font-semibold text-primary-color lg:text-lg"
-          >
-            Leer más »
-          </Link>
-        </article>
-      ))}
+      {!allBlogs.length ? (
+        <p className="font-semibold text-lg border-b-2 border-black">
+          No hay Blogs
+        </p>
+      ) : (
+        blogs.map((art) => (
+          <article key={art.id} data-aos="fade-up" data-aos-duration="2000">
+            <div className="w-full h-56 lg:h-60">
+              <img
+                className="w-full h-full object-cover"
+                src={art.img}
+                alt={art.title}
+              />
+            </div>
+            <h3 className="text-lg font-semibold my-3 lg:text-2xl">
+              {art.title}
+            </h3>
+            <p className="my-3 text-sm text-second-grey lg:text-base">
+              {art.description}
+            </p>
+            <Link
+              to={`/blog/${art.id}`}
+              className="font-semibold text-primary-color lg:text-lg"
+            >
+              Leer más »
+            </Link>
+          </article>
+        ))
+      )}
     </>
   )
 }
