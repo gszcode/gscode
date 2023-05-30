80e970500606fd9e18030ede48aeb8fb86a79dbe
Gabriel <gszcode@gmail.com>
2023-05-30 14:49:42 -0300

    componente blos conectado al back y consultando por todos los blogs

--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -16,9 +16,9 @@ const AllBlogs = () => {
       <h2 className="text-2xl font-bold text-center md:text-4xl pt-28">
         Todos nuestros Blogs
       </h2>
-      <section className="px-3 min-h-screen flex justify-evenly items-center gap-3 flex-wrap">
+      <section className="px-3 min-h-screen flex justify-evenly items-start gap-3 flex-wrap">
         {!allBlogs.length ? (
-          <p className="font-semibold text-lg border-b-2 border-black -mt-40">
+          <p className="font-semibold text-lg border-b-2 border-black mt-40">
             No hay Blogs
           </p>
         ) : (
@@ -27,18 +27,18 @@ const AllBlogs = () => {
               <div className="w-full h-56 lg:h-60">
                 <img
                   className="w-full h-full object-cover"
-                  src={art.img}
-                  alt={art.title}
+                  src={art.image}
+                  alt={`${art.title.slice(0, 15)}...`}
                 />
               </div>
               <h3 className="text-lg font-semibold my-3 lg:text-2xl">
-                {art.title}
+                {`${art.title.slice(0, 20)}...`}
               </h3>
               <p className="my-3 text-sm text-second-grey lg:text-base">
-                {art.description}
+                {`${art.description.slice(0, 25)}...`}
               </p>
               <Link
-                to={`/blog/${art.id}`}
+                to={`/blog/${art._id}`}
                 className="font-semibold text-primary-color lg:text-lg"
               >
                 Leer más »
